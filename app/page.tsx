422852eee6fc5035bf17310c5c565206029a96b4
Jaskaran1519 <jaskaran1519@gmail.com>
2025-03-20 22:15:26 +0530

    sofknso

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import { CarouselDemo } from '@/components/Hero/Carousel'
-import Navbar from '@/components/Header/Navbar'
-import React from 'react'
 import { VelocityScroll } from '@/components/magicui/scroll-based-velocity'
 
-export const Page = () => {
+const Page = () => {
   return (
     <div className='w-full max-w-[2400px]'>
       <div className='pb-8'>
